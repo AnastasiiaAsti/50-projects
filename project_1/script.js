8f4af218f5df0101375940294998cb1402f3be70
Asti <astistateofmind@gmail.com>
2022-08-25 12:58:14 -0700

    add function to expand cards

---
+++ project_1/script.js
@@ -0,0 +1,17 @@
+//grabbing panels
+const panels = document.querySelectorAll('.panel')
+//console.log(panels)
+
+//iterrating over nodeList of panels
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
